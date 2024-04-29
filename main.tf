7f5b29fd8bbe853039e36b50c2f0cf7088bc9c99
Mateusz Jenek <mateusz.jenek@humanitec.com>
2024-04-29 12:31:23 +0200

    Add main file

---
+++ main.tf
@@ -0,0 +1,19 @@
+output "host" {
+    value = "example.com"
+}
+
+output "name" {
+  value = "my-db"
+}
+
+output "port" {
+  value = "6543"
+}
+
+output "username" {
+  value = "john"
+}
+
+output "password" {
+  value = "pass1234"
+}
